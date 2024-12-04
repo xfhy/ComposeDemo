1bcb6e553f5c1d93a578295dcd686acfc2660d17
xfhy <xfhy666@qq.com>
2024-12-04 16:03:49 +0800

    在已有布局中加入Compose View

---
+++ app/src/main/java/com/xfhy/composedemo/OldActivity.kt
@@ -0,0 +1,48 @@
+package com.xfhy.composedemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.xfhy.composedemo.ui.theme.ComposeDemoTheme
+
+/**
+ * 在已有布局中加入Compose View
+ */
+class OldActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_old)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val composeView = findViewById<ComposeView>(R.id.compose_view)
+        composeView.setContent {
+            ComposeDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                        Column {
+                            Text(text = "我是Compose中的View")
+                            Image(painterResource(id = R.drawable.icon_test), contentDescription = "")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
